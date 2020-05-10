a1482dc10e4211ad263312b80fbec3644d58ddea
Rahul Gaur <rgaur1620@gmail.com>
2020-05-10 17:57:01 +0530

    added google authentication

---
+++ app/src/main/java/application/rahul/connectifyme/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package application.rahul.connectifyme.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import application.rahul.connectifyme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/rest/ApiClient.java
@@ -0,0 +1,21 @@
+package rest;
+
+import android.app.Activity;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient extends Activity {
+
+    public static final String BASE_URL ="https://10.0.2.2/connectifyme";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getApiClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
