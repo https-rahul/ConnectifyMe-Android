0b4a7faf28286ee99148627cc687a325b96ed3b4
Rahul Gaur <rgaur1620@gmail.com>
2020-05-15 23:14:08 +0530

    added bottom navigation with animation text pop

--- app/src/main/java/application/rahul/connectifyme/activity/MainActivity.java
+++ app/src/main/java/application/rahul/connectifyme/activity/MainActivity.java
@@ -1,16 +1,49 @@
 package application.rahul.connectifyme.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import application.rahul.connectifyme.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+    @BindView(R.id.search)
+    ImageView search;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.framelayout)
+    FrameLayout framelayout;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        getSupportActionBar(toolbar);
+
+        bottomNavigation.inflateMenu(R.menu.bottom_navigation_main);
+        bottomNavigation.setItemBackgroundResource(R.color.colorPrimary);
+        bottomNavigation.setItemTextColor(ContextCompat.getColorStateList(bottomNavigation.getContext(), R.color.nav_item_colors));
+        bottomNavigation.setItemIconTintList(ContextCompat.getColorStateList(bottomNavigation.getContext(), R.color.nav_item_colors));
+
+
     }
 }
